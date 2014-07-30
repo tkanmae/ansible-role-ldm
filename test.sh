cf463b0d6b0818d6385edcf5a795470cf6792bb7
Takeshi Kanmae <tkanmae@gmail.com>
2014-07-29 21:41:55 -0800

    Abort if Ansible is not installed

--- test.sh
+++ test.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 #
-# Perform a syntax check of this role.
+# Perform a syntax check on this role.
 #
 
 \unalias -a
+
+
+print_error_and_die() {
+    echo "$@"
+    exit 1
+} >&2
+
+
+which ansible >/dev/null 2>&1 || print_error_and_die "Ansible is not installed"
 
 HOSTFILE=/tmp/hosts
 echo localhost >$HOSTFILE
